2fe90c424da81c4db4b58d6015bd4820bb2da042
allakoval <2448572@gmail.com>
2022-10-29 13:05:47 +0300

    lector3

--- lector3/Program.cs
+++ lector3/Program.cs
@@ -79,27 +79,69 @@
 // маленикие буквы "к" заменить большими к
 // больште буквы С заменить маленькими с
 
-string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
-            + "ежели бы вас послали вместо нашего милого"
-            + "Винценгероде, вы бы взяли приступом согласие"
-            + "прусского короля. Вы так красноречивы. Вы"
-            + "дадите мне чаю?";
+// string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+//             + "ежели бы вас послали вместо нашего милого"
+//             + "Винценгероде, вы бы взяли приступом согласие"
+//             + "прусского короля. Вы так красноречивы. Вы"
+//             + "дадите мне чаю?";
 
 // string = "qwert"
 //           01234   
 // s[3] // r
 
-string Replace(string text, char oldValue,char newValue)
+// string Replace(string text, char oldValue,char newValue)
+// {
+//  string result = String.Empty;
+//  int length = text.Length;
+//  for(int i = 1; i < 10; i++)
+//  {
+//     if(text[1] == oldValue) result =result + $"{newValue}";
+//     else result = result + $"{text[i]}";
+//  }
+//  return result;
+// }
+
+// string textnew = Replace(text, ' ', '|');
+// Console.WriteLine(textnew);
+
+
+
+
+
+// Задача сортировка
+
+int[] arr = {1, 5, 4, 3, 2, 6,7 ,1 ,1};
+void PrintArray(int[] array)// выводит вест массив
 {
- string result = String.Empty;
- int length = text.Length;
- for(int i = 1; i < 10; i++)
- {
-    if(text[1] == oldValue) result =result + $"{newValue}";
-    else result = result + $"{text[i]}";
- }
- return result;
+   int count = array.Length;
+   for(int i = 0; i < count; i++)
+   {
+      Console.Write($"{array[i]} ");
+   }
+   Console.WriteLine();
 }
 
-string textnew = Replace(text, ' ', '|');
-Console.WriteLine(textnew);
+void SelectionSort(int[] array)// упорядочивает массив
+{
+ for(int i = 0; i < array.Length-1; i++)// тут добавили в длину -1, что бв ыо внутренем чикце, где ай+1, проходил весь массив
+ {
+   int minPosition = i;
+
+   for(int j = i + 1; j < array.Length; j++)
+   {
+    if(array[j]< array[minPosition]) minPosition = j;
+   }
+   
+   int temporary = array[i];//вводим переменную  и соъраняем туда первое хначение
+   array[i] = array[minPosition];// первому значение передаем значение мин
+   array[minPosition] = temporary;// мин передаем хначение темп
+
+
+
+
+
+ }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
